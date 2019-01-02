989be5b2101098eccf79a6221926e22fb2661f0e
Ivan Dimitrov <ivan.dimitrov@telerik.com>
2019-01-02 17:30:10 +0200

    Update AssemblyInfo.cs

--- NewsRotator/Properties/AssemblyInfo.cs
+++ NewsRotator/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Web.UI;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Progress")]
 [assembly: AssemblyProduct("Progress Sitefinity CMS")]
-[assembly: AssemblyCopyright("© 2016 Telerik AD. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2005-2019 Progress Software Corporation and/or one of its subsidiaries or affiliates. All rights reserved.")]
 [assembly: AssemblyTrademark("Sitefinity")]
 [assembly: AssemblyCulture("")]
 
